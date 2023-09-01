8a589c175477748c8ed06ad17e33990800f74ec3
Taahia23 <taahiatahsin@gmail.com>
2023-09-02 02:07:41 +0600

    button added

--- js/tube.js
+++ js/tube.js
@@ -1,1 +1,34 @@
-console.log('hello i am js');
+const loadCategory  = async() => {
+    const res = await fetch(' https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+     
+    const categories = data.data;
+    console.log(categories);
+     for(category of categories){
+        const cate = category.category;
+        console.log(cate);
+    }
+    displayCateName(categories)
+}
+
+const displayCateName = (categories) => {
+    const categoryName = document.getElementById('cate-name');
+   
+    categories.forEach(category => {
+        const cateBUtton = document.createElement('button');
+    cateBUtton.classList = ``;
+     const cate = category.category;
+    // console.log(cate);
+    cateBUtton.innerHTML = 
+    `
+    <button class="btn btn-gray mr-9 px-9">${cate}</button>
+    `
+    categoryName.appendChild(cateBUtton);
+    });
+
+    
+}
+
+
+
+loadCategory();
